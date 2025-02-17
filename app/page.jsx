b5b8d8d9963b311d93deaaf14b20b78807e54651
Rhishavhere <rhishh.me@gmail.com>
2025-02-17 22:33:44 +0530

    deploy fix

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@
 import Image from "next/image"
 import { motion, useScroll, useTransform } from "framer-motion";
 import { useState, useEffect } from "react"
-// import dynamic from "next/dynamic";
+import dynamic from "next/dynamic";
 
 import Cursor from "@/components/Cursor"
 import Links from "@/components/Links";
@@ -10,11 +10,11 @@ import Nav from "@/components/Nav";
 import Main from "@/components/Main";
 import Info from "@/components/Info";
 import Skills from "@/components/Skills";
-import AiProj from "@/components/AiProj";
+// import AiProj from "@/components/AiProj";
 
-// const DynamicAiProj = dynamic(() => import("@/components/AiProj"), {
-//   ssr: false
-// });
+const DynamicAiProj = dynamic(() => import("@/components/AiProj"), {
+  ssr: false
+});
 
 export default function Home() {
   return (
@@ -26,8 +26,8 @@ export default function Home() {
     <Main/>
     <Info/>
     <Skills/>
-    <AiProj/>
-    {/* <DynamicAiProj/> */}
+    {/* <AiProj/> */}
+    <DynamicAiProj/>
         
   
     <div className="text-black">
